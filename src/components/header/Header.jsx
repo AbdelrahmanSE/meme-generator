890110238e1410e2e7483e14318570dc65c7cec0
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-04-05 01:07:05 +0200

    Files Renaming and Created Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function Header() {
+  return (
+    <header>
+      <div className="ui stackable menu inverted segment">
+        <div className="ui container">
+          <div className="item">
+            <i class="image outline icon" />
+            Meme Generator
+          </div>
+          <a
+            className="item"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/AbdelrahmanSE/meme-generator"
+          >
+            View on GitHub
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
